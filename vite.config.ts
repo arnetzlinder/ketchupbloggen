385570dbf6c84320e5db7be1ad5407162926bbb7
jenmwa <113125376+jenmwa@users.noreply.github.com>
2023-01-10 23:16:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/fed22d-grafiska-verktyg-ketchup/', 
+  define: {
+    'process.env': {},
+  },
 })
