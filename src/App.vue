7264c82ae3bfdd67ecdfd1e1f9a0f016d9ca19e2
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-01-10 20:11:40 +0100

    :lipstick: Footer component added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import FooterSection from './components/FooterSection.vue';
 </script>
 
 <template>
+    <FooterSection />
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+
+</style>
