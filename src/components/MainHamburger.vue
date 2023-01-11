1b2e39c71d83cbc4c09c0394419e2af9504d1e47
jenmwa <jenmwa@gmail.com>
2023-01-11 17:13:31 +0100

    :lipstick: Menu Btn css

--- src/components/MainHamburger.vue
+++ src/components/MainHamburger.vue
@@ -1,10 +1,42 @@
-<script lang="ts">
-</script>
-
 <template>
-    <button class="material-symbols-outlined">menu
+    <button @click="menuOpenClose">Meny
     </button>
 </template>
 
+<script lang="ts">
+export default {
+    methods: {
+        menuOpenClose() {
+            console.log('clicketi')
+        }
+    } 
+}
+// function menuOpenClose() {
+//     console.log('klick');
+// }
+</script>
+
 <style scoped lang="scss">
+
+button {
+    background-color: #C41A00;
+    color: #FFF3D0;
+    font-family: 'Istok Web', sans-serif;
+    font-size: 1rem;
+    width: 76px;
+    height: 25px;
+    padding: 1.5rem 1rem;
+    border: 3px solid black; 
+    border-radius: 15px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+
+    &:hover {
+        background-color: #AA1700;
+    }
+    &:active {
+        background-color: #650D00;
+    }
+}
 </style>
