8b2b1d94cceb4efd0880f55c5b07a0a653fa5c90
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-01-11 13:31:00 +0100

    Co-authored-by: jenmwa <jenmwa@users.noreply.github.com> Co-authored-by: Sussie Arnetz Linder <arnetzlinder@users.noreply.github.com>

---
+++ src/components/ImagePlay.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button> </button>
+        <img: src="''+images[]" alt="recepies[]alt" width="300" height="200" loading="lazy">
+        <button> </button>
+
+
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
