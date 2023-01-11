33dc931c8c4722a7347dbe5fd8e8569be89c5664
jenmwa <jenmwa@gmail.com>
2023-01-11 18:42:43 +0100

    :poop: colorChange nav-section

--- src/components/MainNavBar.vue
+++ src/components/MainNavBar.vue
@@ -11,14 +11,22 @@ console.log(isMenuOpen);
 </script>
 
 <template>
-    <nav>
-    <MainHamburger @menu-click="toggleMenu"/>
-        <ul v-if="showMenu">
-            <header>Meny</header>
+    <header>
+        <nav>
+         <MainHamburger @menu-click="toggleMenu"/>
+            <ul v-if="showMenu">
+                <h4>Meny</h4> <!--kolla figma vilken typ av h! -->
                 <li>Historia</li>
                 <li>Förrätter</li>
                 <li>Huvudrätter</li>
                 <li>Efterrätter</li>
             </ul>
-    </nav>
+        </nav>
+    </header>
 </template>
+
+<style scoped lang="scss">
+nav {
+    background-color: white;
+}
+</style>
