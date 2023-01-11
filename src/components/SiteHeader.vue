cdeb1abf92f313bd3b56b2721aacfe8c5abf33da
robin-sevelin <robinsevelin@hotmail.com>
2023-01-11 10:18:28 +0100

    header img search path

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -9,36 +9,30 @@
 <style scoped lang="scss">
 @forward '../scss/style.scss';
 
-
-
 @media screen and (min-width: 360px) {
     header{
     border: 1px solid black;
     height: 195px;
     color: #FFF3D0;;
-    background-image: url('assets/images/KetchupbloggenHeaderImg-Mobile.webp');
+    background-image: url('../../assets/images/KetchupbloggenHeaderImg-Mobile.webp');
     background-size: cover;
     background-repeat: no-repeat;
     .h1-container{
         padding: 11px;
-
         h1{
         font-size: 32px;
     }
     }
 
 }
-
     }
 
     @media screen and (min-width: 744px) {
         header{
             height: 322px;
-            background-image: url('assets/images/KetchupbloggenHeaderImg-Tablet.webp');
+            background-image: url('../../assets/images/KetchupbloggenHeaderImg-Tablet.webp');
             .h1-container{
         padding: 20px;
-
-
         h1{
         font-size: 64px;
         margin-top: 20px;
@@ -48,16 +42,13 @@
         }
 
     }
-
     @media screen and (min-width: 1440px) {
         header{
             height: 436px;
-            background-image: url('assets/images/KetchupbloggenHeaderImg-Desktop.webp');
+            background-image: url('../../assets/images/KetchupbloggenHeaderImg-Desktop.webp');
             .h1-container{
 
         padding: 34px;
-
-
         h1{
         font-size: 64px;
     }
