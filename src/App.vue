f24ae2cf0579ab0e25d56708815a42b33951277e
jenmwa <jenmwa@gmail.com>
2023-01-11 15:27:03 +0100

    :art: btn added menu

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import SiteHeader from './components/SiteHeader.vue';
 import FooterSection from './components/FooterSection.vue';
+import MainHamburger from './components/MainHamburger.vue';
 
 import recepies from './components/array.ts';
 </script>
 
 <template>
     <SiteHeader />
+    <MainHamburger />
     <div class="articles">
         <ArticleCard v-for="(recipe, index) in recepies" :card-data="recipe" :key="index" />
     </div>
---
+++ src/components/MainHamburger.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<template>
+    <button class="material-symbols-outlined">menu
+    </button>
+</template>
+
+<style scoped lang="scss">
+</style>
