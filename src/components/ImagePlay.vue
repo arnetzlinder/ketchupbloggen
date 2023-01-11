04ad82ee1417d0a8b42f9e992cc4cfbb3d269454
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-01-11 15:00:35 +0100

    Co-authored-by: jenmwa <jenmwa@users.noreply.github.com> Co-authored-by: Sussie Arnetz Linder <arnetzlinder@users.noreply.github.com> Co-authored-by: Jenni <postmodernistx@users.noreply.github.com>

--- src/components/ImagePlay.vue
+++ src/components/ImagePlay.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
-        <button> </button>
-        <img: src="''+images[]" alt="recepies[]alt" width="300" height="200" loading="lazy">
-        <button> </button>
-
-
+        <button @click="goBackward" class="material-symbols-outlined">
+        arrow_back_ios
+        </button>
+        <img :src="'../../assets/images'+images[this.currentIndex]" alt="recepies[]alt" width="300" height="200" loading="lazy" />
+        <button @click="goForward" class="material-symbols-outlined">
+        arrow_forward_ios 
+        </button>
     </div>
 </template>
 
--- src/components/array.ts
+++ src/components/array.ts
@@ -1,16 +1,20 @@
-let recepies: {id: number; name: string; ingredient: string; desc: string; images: {img1: string; img2: string; img3: string; alt1: string; alt2: string; alt3: string;}[] } [] = [
+let recepies: {id: number; name: string; ingredient: string; desc: string; images: {img: string; alt: string;}[] } [] = [
     //Array which stores all info about the recepies, e.g. name
     {
       id: 1,
       images: [
-        {          
-          img1: "../../assets/images/Ketchupmarinad-1.webp",
-          img2: "../../assets/images/Ketchupmarinad-2.webp",
-          img3: "../../assets/images/Ketchupmarinad-3.webp",
-          alt1: "Ketchupmarinadflaska på bakgrund av tomater",
-          alt2: "Ketchupmarinadflaska och skål med tomater",
-          alt3: "Ketchupmarinad i skål",
+        {
+          img: "../../assets/images/Ketchupmarinad-1.webp",
+          alt: "Ketchupmarinadflaska på bakgrund av tomater",
         },
+        {
+          img: "../../assets/images/Ketchupmarinad-2.webp",
+          alt: "Ketchupmarinadflaska och skål med tomater",
+        },
+        {
+          img: "../../assets/images/Ketchupmarinad-3.webp",
+          alt: "Ketchupmarinad i skål",
+        }
       ],
       name: "Ketchupmarinad",
       ingredient: "",
@@ -20,13 +24,17 @@ let recepies: {id: number; name: string; ingredient: string; desc: string; image
       id: 2,
       images: [
         {
-          img1: "../../assets/images/Ketchupsoppa-1.webp",
-          img2: "../../assets/images/Ketchupsoppa-2.webp",
-          img3: "../../assets/images/Ketchupsoppa-3.webp",
-          alt1: "Ketchupsoppa i skål med kvistar av timjan",
-          alt2: "Flera skålar med varierande innehåll däribland ketchupssoppa",
-          alt3: "Ketchupsoppa med kvist av bladpersilja",
+          img: "../../assets/images/Ketchupsoppa-1.webp",
+          alt: "Ketchupsoppa i skål med kvistar av timjan",
         },
+        {
+          img: "../../assets/images/Ketchupsoppa-2.webp",
+          alt: "Flera skålar med varierande innehåll däribland ketchupssoppa",
+        },
+        {
+          img: "../../assets/images/Ketchupsoppa-3.webp",
+          alt: "Ketchupsoppa med kvist av bladpersilja",
+        }
       ],
       name: "Ketchupsoppa",
       ingredient: "4 msk Smör, 1 st Gul lök, 1 kg Tomater, 1 st Lagerblad, 4 kvistar Basilika, 4 kvistar Persilja, 2 liter Grönsaksbuljong, 1 msk Tomatketchup, 1 dl Vispgrädde",
@@ -36,13 +44,17 @@ let recepies: {id: number; name: string; ingredient: string; desc: string; image
       id: 3,
       images: [
         {
-          img1: "../../assets/images/Gazpacho-1.webp",
-          img2: "../../assets/images/Gazpacho-2.webp",
-          img3: "../../assets/images/Gazpacho-3.webp",
-          alt1: "Tomat",
-          alt2: "Skål med Gazpacho med gröna kvistar och brödskiva",
-          alt3: "Gazpacho hälls upp i skål från glasflaska",
+          img: "../../assets/images/Gazpacho-1.webp",
+          alt: "Tomat",
         },
+        {
+          img: "../../assets/images/Gazpacho-2.webp",
+          alt: "Skål med Gazpacho med gröna kvistar och brödskiva",
+        },
+        {
+          img: "../../assets/images/Gazpacho-3.webp",
+          alt: "Gazpacho hälls upp i skål från glasflaska",
+        }
       ],
       name: "Gazpacho",
       ingredient: "",
