dd24411b26fd37ed3d5d0e4582f53476eda47413
robin-sevelin <robinsevelin@hotmail.com>
2023-01-13 09:14:14 +0100

    :sparkles: add icon with rotation + greensock

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,8 @@
 <template>
+    <div class="nav-container">
     <HamburgerBtn  @menu-click="toggleMenu"/> <!-- Här kopplas klick-eventet "menu-click" ihop från komponenten "HamburgerBtn" (se rad 16 i HamburgerBtn.vue) @-tecken används för att koppla event. -->
+    <MagnifyingGlass /> <!--här kopplar vi in förstoringsglas komponenten-->
+    </div>
     <MainMenu :is-open="showMenu"/> <!-- Här kopplar vi ihop if-satsen från komponenten "MainMenu.vue", det görst med ett :-tecken, vilket är en förkortning av v-bind. -->
 </template>
 
@@ -10,12 +13,14 @@
 
 
 <script lang="ts">
+    import MagnifyingGlass from './MagnifynGlass.vue';
     import HamburgerBtn from './HamburgerBtn.vue';
     import MainMenu from './MainMenu.vue';
     export default {
         components: {       // components-deklarationen gör att vi kan använda dess props i vår template-tagg.
             HamburgerBtn,
             MainMenu,
+            MagnifyingGlass,
         },
         data() {
             return {
@@ -39,4 +44,9 @@
 
 
 <style scoped lang="scss">
+.nav-container{
+    display: flex;
+    justify-content: space-between;
+
+}
 </style>
