76d3293797645728c21ca95d3de1e326db0b0f66
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-01-16 22:26:21 +0100

    :lipstick: add borderradius and gap to articlecard

--- src/components/ShowRecepie.vue
+++ src/components/ShowRecepie.vue
@@ -2,8 +2,9 @@
     <div class="show-recepie">
         <img :src="cardData.images[0].img" :alt="cardData.images[0].alt" width="236" height="153" loading="lazy" />
         <h2 v-html="cardData.name"> </h2>
-
+        <h3> Ingredienser:</h3>
         <div v-html="cardData.ingredient" class="desc"></div>
+        <h3> Gör så här:</h3>
         <div v-html="cardData.desc" class="desc">
         </div>
         <button @click="recepieHide" class="closeButton" inheritAttrs: false >Stäng</button>
