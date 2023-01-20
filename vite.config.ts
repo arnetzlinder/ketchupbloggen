21a5e480b7ada9fc14a85202182380ebe657f2cc
Susanne Arnetz Linder <susanne.arnetzlinder@medieinstitutet.se>
2023-01-20 10:46:56 +0100

    igyi

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/fed22d-grafiska-verktyg-ketchup/',
+  base: '/ketchupbloggen/',
   define: {
     'process.env': {},
   },
